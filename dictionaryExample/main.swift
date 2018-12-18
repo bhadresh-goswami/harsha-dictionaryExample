6b6765818bfd7b2c4d6ff25112dbeb5566e0ba57
tops-bhadresh <tops.bhadresh@gmail.com>
2018-12-18 05:18:22 -0800

    done1

--- dictionaryExample/main.swift
+++ dictionaryExample/main.swift
@@ -8,5 +8,29 @@
 
 import Foundation
 
-print("Hello, World!")
+//immutable
+//not able to work with this
+//let dict = [String:Any]()
+let dict = ["Name":"Bhadresh","Course":"iOs","Age":30] as [String : Any]
+//dict["Name"] = "harsha"
+print("The value of name is \(dict["Name"]!)")
+
+//mutable
+var dict1 = [String:Any]()//init, init to default memory
+var d2:[String:Any]
+d2 = [String:Any]()
+d2["Name"] = "aa"
+
+var d3 = ["Name":"Bhadresh","Course":"iOs","Age":30] as [String : Any]
+d3["Name"] = "Rajesh Nagar"
+d3["Course"] = "PHP"
+
+let keys = d3.keys
+
+for k in keys{
+    print("\(k):\(d3[k]!)")
+}
+
+
+d3.removeAll()
 
